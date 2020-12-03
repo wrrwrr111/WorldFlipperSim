9222f08b5fd1d8cf699ce076f35845dd0dd07880
baze <wrrwrr111@gmail.com>
2020-12-03 19:49:44 +0800

    change file structure

---
+++ main/Main.gd
@@ -0,0 +1,119 @@
+extends Control
+
+const SilverPin = preload('silver_pin.tscn')
+const GoldPin = preload('gold_pin.tscn')
+const RainbowPin = preload('rainbow_pin.tscn')
+const Ball = preload('ball/ball.tscn')
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var camera = $Camera2D
+
+
+export (int) var silver_chance = 70
+export (int) var gold_min = 3
+export (int) var gold_more = 6
+export (int) var rainbow_min = 0
+export (int) var rainbow_more = 3
+export (bool) var full_auto = false
+var sum = 0
+var silver_num = 0
+var gold_num = 0
+var rainbow_num = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$CanvasLayer/silver_chance.value = silver_chance
+	$CanvasLayer/gold_more.value = gold_more
+	$CanvasLayer/rainbow_more.value = rainbow_more
+
+	
+var pin_arr = []
+var ball
+
+func set_ball(pos):
+	for i in pin_arr:
+		if i:i.free()
+	pin_arr = []
+	if ball:ball.queue_free()
+	camera.set_position(Vector2(0,0))
+	
+	ball = Ball.instance()
+	ball.connect('ball_fall',self,"_on_ball_fall")
+	ball.set_position(pos)
+	add_child(ball)
+	for i in 4:
+		for j in 15:
+			if randi()%100 < silver_chance:
+				var silver_pin = SilverPin.instance()
+				var x = 48+150*i + (j%2)*75
+				var y = 500+100*j
+				silver_pin.set_position(Vector2(x,y))
+				add_child(silver_pin)
+				pin_arr.append(silver_pin)
+	for i in randi()%gold_more:
+		var gold_pin = GoldPin.instance()
+		gold_pin.set_position(Vector2(randi()%600,800+randi()%1200))
+		add_child(gold_pin)
+		pin_arr.append(gold_pin)
+	for i in randi()%rainbow_more:
+		var rainbow_pin = RainbowPin.instance()
+		rainbow_pin.set_position(Vector2(randi()%600,1200+randi()%800))
+		add_child(rainbow_pin)
+		pin_arr.append(rainbow_pin)
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			if(event.position.x >0 && event.position.x<640 && event.position.y<300):
+				set_ball(event.position)
+
+func change_rainbow():
+	for i in pin_arr:
+		i.queue_free()
+	pass
+
+func _on_ball_fall(ball):
+	var pos = ball.position
+	if pos.y > 500 && pos.y < 1500:
+		camera.set_position(Vector2(0,pos.y-500))
+	elif pos.y > 2000:
+		match ball.type:
+			'silver_ball':
+				silver_num += 1
+			'gold_ball':
+				gold_num += 1
+			'rainbow_ball':
+				rainbow_num += 1
+		sum += 1
+		var text = 'sum: '+str(sum)
+		text += '\nsilver_num: '+ str(silver_num)
+		text += '\ngold_num: '+ str(gold_num)
+		text += '\nrainbow_num: '+str(rainbow_num)
+		text += '\nrainbow_rate: '+str(float(rainbow_num)/float(sum))
+		$CanvasLayer/Label6.text = text
+		ball.queue_free()
+		if full_auto:
+			set_ball(Vector2(randi()%640,randi()%300))
+
+
+func _on_silver_chance_value_changed(value):
+	silver_chance = int(value)
+	pass # Replace with function body.
+
+
+
+func _on_gold_more_value_changed(value):
+	gold_more = int(value)
+	pass # Replace with function body.
+
+
+
+func _on_rainbow_more_value_changed(value):
+	rainbow_more = int(value)
+	pass # Replace with function body.
+
+
+func _on_CheckBox_toggled(button_pressed):
+	full_auto = button_pressed
+	pass # Replace with function body.
---
+++ main/ball/ball.gd
@@ -0,0 +1,84 @@
+extends KinematicBody2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+export (int) var gold_chance = 10
+export var type = 'silver_ball' # gold rainbow
+signal ball_fall
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if randi()%100 < gold_chance:
+		$AnimatedSprite.play('gold')
+		type = 'gold_ball'
+	else:
+		$AnimatedSprite.play('silver')
+		type = 'silver_ball'
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+export (int) var gravity = 1000
+var time_speed = 1
+var speed = 0
+var ori_velocity = Vector2()
+var velocity = Vector2()
+func _physics_process(delta):
+	
+	speed += gravity * delta 
+	ori_velocity.y += speed * delta
+	
+	var near_nodes_arr = get_layer_node(180,get_position())
+	time_speed = 1
+	for i in near_nodes_arr:
+		if i.collider.type == 'gold' || i.collider.type == 'rainbow':
+			if(position.y < i.collider.position.y):
+				time_speed = 0.3
+			
+	velocity = ori_velocity * time_speed
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		match collision.collider.type:
+			'silver':
+				ori_velocity = velocity.bounce(collision.normal) * 0.5
+			'gold':
+				if(type == 'silver_ball'):
+					type = 'gold_ball'
+					$AnimatedSprite.play('gold')
+				else:
+					change_rainbow()
+			'rainbow':
+				change_rainbow()
+			'wall':
+				ori_velocity = velocity.bounce(collision.normal) * 0.5
+		if collision.collider.has_method("hit"):
+			collision.collider.hit()
+		
+		
+	emit_signal('ball_fall',self)
+
+
+
+func change_rainbow():
+	type = 'rainbow_ball'
+	$AnimatedSprite.play('rainbow')
+	self.get_parent().change_rainbow()
+
+var shape_cicle := CircleShape2D.new()
+func get_layer_node(radius, pos:Vector2):
+	# 设置碰撞形状
+	shape_cicle.radius = radius
+	
+	# 新建 Physics2DShapeQueryParameters 并配置
+	var p = Physics2DShapeQueryParameters.new()
+	p.set_shape(shape_cicle)		# 设置碰撞形状
+	p.collide_with_areas = true		# 监测 Area 区域
+	p.collision_layer = 1			# 对应被检测物体的 layer 层级
+	p.transform = Transform2D(0, pos)	# 设置碰撞的位置	
+	
+	# 检测碰撞
+	var arr = get_world_2d().direct_space_state.intersect_shape(p)
+	return arr	# 返回碰撞结果
